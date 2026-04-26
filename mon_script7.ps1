7e79dd8c7b7b74f6f86bc16c0992145bb9c14e20
ejarraud166-a11y <ejarraud166@gmail.com>
2026-04-26 20:21:30 +0200

    V1.2 mon_script7.ps1
    Ajout de Do While, d'un Switch, d'une expression de remplacement de chaîne regex pour extraire le réseau de base d'une adresse IP et d'une nouvelle fonctionnalité pour celui-ci (lister les adresses IP d'un fichiers texte)

--- mon_script7.ps1
+++ mon_script7.ps1
@@ -4,8 +4,16 @@
 #ce qui peut être pratique dans le cas où nous voulons a attribuer une adresse
 #à un poste mais que nous ne savons pas qu'elles adresses sont déja occupé.
 
-$LocalIP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "*Ethernet*","*Wi-Fi*" | 
-    Where-Object { $_.IPAddress -notlike "169.*" } | 
-    Select-Object -First 1).IPAddress
-    $ReportFile = Join-Path -Path $PSScriptRoot -ChildPath "rapport_ip.txt"
-    Add-Content -Path $ReportFile -Value "$LocalIP"
+Do {
+Write-Host "`n1- Lancer le scan réseau `n2- Lister les adresses d un fichier 3- Sortir du script"
+$Number = Read-Host "Choisir un chiffre entre 1 et 2 (3 pour sortir)"
+switch ($Number) {
+        
+        1 { $LocalIP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "*Ethernet*","*Wi-Fi*" | 
+            Where-Object { $_.IPAddress -notlike "169.*" } | 
+            Select-Object -First 1).IPAddress
+        $ReportFile = Join-Path -Path $PSScriptRoot -ChildPath "rapport_ip.txt"
+        Add-Content -Path $ReportFile -Value "$LocalIP"
+        $Plage = $LocalIP -replace "\.\d+$" }
+ 
+} While ($Number -ne 3) 
