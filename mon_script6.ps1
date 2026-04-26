6a3beca666f2f0194381fa8f0e057946e1999f7d
ejarraud166-a11y <ejarraud166@gmail.com>
2026-04-26 15:34:42 +0200

    V1 mon_script6.ps1

--- mon_script6.ps1
+++ mon_script6.ps1
@@ -3,4 +3,19 @@ if (-not (Test-Path -Path $FilePath)) {
   Write-Warning "Fichier introuvable : $FilePath"
 }
 $Machines = Get-Content -Path $FilePath
-$ReportFile = Join-Path -Path $PSScriptRoot -ChildPath "rapport_machines.txt"
+$Date = Get-Date -Format "dd/MM/yyyy_HH/mm/ss"
+$ReportFile = Join-Path -Path $PSScriptRoot -ChildPath "rapport_machines_$Datee.txt"
+New-Item -Path $ReportFile -ItemType File | Out-Null
+Write-Host "`nFichier de rapport cree : $ReportFile"
+
+foreach ($Machines in $Machines) {
+  Write-Host "Le ping de la machine $Machines est en cours..."
+  $Ping = Test-Connection $Machines -Count 1 -Quiet -ErrorAction SilentlyContinue
+
+  if ($Ping -eq $true) {
+    Add-Content "rapport_machines_$Datee.txt" "$Machines est JOIGNABLE"
+  }
+  else {
+    Add-Content "rapport_machines_$Datee.txt" "$Machines est JOIGNABLE"
+  }
+}
