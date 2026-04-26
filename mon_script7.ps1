004f1afe5acdac2da323facf411a3b8a9eb7cb96
ejarraud166-a11y <ejarraud166@gmail.com>
2026-04-26 20:01:29 +0200

    V1 mon_script7.ps1
    Test de la commande Get-NetIPAddress

--- mon_script7.ps1
+++ mon_script7.ps1
@@ -1,4 +1,11 @@
 #J'ai choisi de faire un script qui permet de faire un scan réseau.
+#Il effectuera un scan réseau et enregistrera toutes les ip dans un fichier txt
 #Pouvant ainsi permettre la détection d'adresse non utilisé dans le réseau,
 #ce qui peut être pratique dans le cas où nous voulons a attribuer une adresse
 #à un poste mais que nous ne savons pas qu'elles adresses sont déja occupé.
+
+$LocalIP = (Get-NetIPAddress -AdressFamily IPv4 -InterfaceAlias "*Ethernet*","*Wi-Fi*" | 
+    Where-Object { $_.IPAddress -notlike "169.*" } | 
+    Select-Object -First 1).IPAddress
+    $ReportFile = Join-Path -Path $PSScriptRoot -ChildPath "rapport_ip.txt"
+    Add-Content -Path $ReportFile -Value "$LocalIP"
