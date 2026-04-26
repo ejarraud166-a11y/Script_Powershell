23ff2326c2c2c620f02170cc7bb5d49a4c3af3d5
ejarraud166-a11y <ejarraud166@gmail.com>
2026-04-26 16:03:15 +0200

    V2 mon_script6.ps1

--- mon_script6.ps1
+++ mon_script6.ps1
@@ -1,21 +1,31 @@
 $FilePath = Read-Host "Entrez le chemin du fichier texte contenant la liste des noms des machines"
 if (-not (Test-Path -Path $FilePath)) {
   Write-Warning "Fichier introuvable : $FilePath"
+  exit
 }
 $Machines = Get-Content -Path $FilePath
-$Date = Get-Date -Format "dd/MM/yyyy_HH/mm/ss"
-$ReportFile = Join-Path -Path $PSScriptRoot -ChildPath "rapport_machines_$Datee.txt"
-New-Item -Path $ReportFile -ItemType File | Out-Null
+$Date = Get-Date -Format "dd-MM-yyyy_HH-mm-ss"
+$ReportFile = Join-Path -Path $PSScriptRoot -ChildPath "rapport_machines_$Date.txt"
+New-Item -Path $ReportFile -ItemType File -Force | Out-Null
 Write-Host "`nFichier de rapport cree : $ReportFile"
 
-foreach ($Machines in $Machines) {
-  Write-Host "Le ping de la machine $Machines est en cours..."
-  $Ping = Test-Connection $Machines -Count 1 -Quiet -ErrorAction SilentlyContinue
+$Joignable = 0
+$NJoignable = 0
+
+foreach ($Machine in $Machines) {
+  Write-Host "Le ping de la machine $Machine est en cours..."
+  $Ping = Test-Connection $Machine -Count 1 -Quiet -ErrorAction SilentlyContinue
 
   if ($Ping -eq $true) {
-    Add-Content "rapport_machines_$Datee.txt" "$Machines est JOIGNABLE"
+    Write-Host "$Machine est JOIGNABLE"
+    Add-Content "rapport_machines_$Datee.txt" "$Machine est JOIGNABLE"
+    $Joignable++
   }
   else {
-    Add-Content "rapport_machines_$Datee.txt" "$Machines n'est pas JOIGNABLE"
+    Write-Host "$Machine n'est pas JOIGNABLE"
+    Add-Content "rapport_machines_$Date.txt" "$Machine n'est pas JOIGNABLE"
+    $NJoignable++
   }
 }
+Write-Host "Total de machine JOIGNABLE : $Joignable"
+Write-Host "Total de machine non JOIGNABLE : $NJoignable"
