a480deac74fca66f2b51f2aa42ac281ee890a1d0
ejarraud166-a11y <ejarraud166@gmail.com>
2026-04-26 19:51:38 +0200

    Create mon_script7.ps1
    Explication de ce que va réaliser mon script 7

---
+++ mon_script7.ps1
@@ -0,0 +1,4 @@
+#J'ai choisi de faire un script qui permet de faire un scan réseau.
+#Pouvant ainsi permettre la détection d'adresse non utilisé dans le réseau,
+#ce qui peut être pratique dans le cas où nous voulons a attribuer une adresse
+#à un poste mais que nous ne savons pas qu'elles adresses sont déja occupé.
